d9d75749ebe81ff617d2b9998569b86211e40569
Samanja Cartagena <c.samanja09@Pabs.local>
2025-05-13 22:05:33 -0400

    stars created

---
+++ src/components/StarBackground.jsx
@@ -0,0 +1,10 @@
+import {useState} from 'react'
+//id, size, x, y, opacity, animationDuration
+
+export const StarBackground = () => {
+      const [stars, setStars] = useState([])
+      const generateStars = () =>{
+        const numberOfStars = Math.floor(window.innerWidth * window.innerHeight/10000)
+      }
+    return <div></div>
+}
